c1b2f4b9c37c48599f6eefc4e74c76f7f0fe4d68
Gustavo Landfried <gustavolandfried@gmail.com>
2022-03-31 14:41:11 -0300

    Agriculturas, egipto, chaco

---
+++ egiptos.sh
@@ -0,0 +1,5 @@
+convert egipto1.jpeg -crop x1050+0+200 output/egipto1.jpeg
+convert egipto2.jpeg -crop x1050+0+200 output/egipto2.jpeg
+convert egipto3.jpeg -crop x1050+0+200 output/egipto3.jpeg
+convert egipto4.jpeg -crop x1050+0+200 output/egipto4.jpeg
+
